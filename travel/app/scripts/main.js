53a05f0a468b40b9213bd6a6533419b30fc3f1e5
Lawrence Humphrey <lawrenceh1993@gmail.com>
2016-09-25 14:45:04 -0500

    filtering is working dog

--- travel/app/scripts/main.js
+++ travel/app/scripts/main.js
@@ -8,9 +8,57 @@ let $ = (query) => {
   return res;
 };
 
+let $things = $('.thing');
+
 $('.filtered-things').addEventListener('click', (e) => {
   let article = e.target.parentNode.parentNode;
   article = article.querySelector('article');
   article.classList.toggle('hidden');
   article.classList.toggle('fadeIn');
+});
+
+$('div.districts').addEventListener('click', (e) => {
+  let district = e.target.dataset.district;
+  let updateThings = () => {
+    let filter = (elem) => {
+      let loc = elem.querySelector('h5');
+      if (loc.innerText.toLowerCase().indexOf(district) >= 0) {
+        return elem;
+      }
+      return null;
+    };
+
+    let filtered = _.filter($things, filter);
+    let container = $('.filtered-things ul');
+
+
+    let removeChildren = (c) => {
+      while (c.firstChild) {
+        c.removeChild(c.firstChild);
+      }
+    };
+
+    removeChildren(container);
+    _.map(filtered, (elem) => { container.appendChild(elem); });
+  };
+
+  let updateString = () => {
+    let string = $('span.district');
+
+    string.classList.add('active');
+    string.innerText = district;
+  };
+
+  if(e.target && e.target.nodeName == 'IMG') {
+    updateThings();
+    updateString();
+  }
+});
+
+$('nav .districts.dropdown').addEventListener('click', (e) => {
+  $('div.districts').classList.toggle('hidden');
+});
+
+$('nav .activities.dropdown').addEventListener('click', (e) => {
+  $('div.activities').classList.toggle('hidden');
 });
--- travel/dist/scripts/main.js
+++ travel/dist/scripts/main.js
@@ -10,10 +10,59 @@ var $ = function $(query) {
   return res;
 };
 
+var $things = $('.thing');
+
 $('.filtered-things').addEventListener('click', function (e) {
   var article = e.target.parentNode.parentNode;
   article = article.querySelector('article');
   article.classList.toggle('hidden');
   article.classList.toggle('fadeIn');
+});
+
+$('div.districts').addEventListener('click', function (e) {
+  var district = e.target.dataset.district;
+  var updateThings = function updateThings() {
+    var filter = function filter(elem) {
+      var loc = elem.querySelector('h5');
+      if (loc.innerText.toLowerCase().indexOf(district) >= 0) {
+        return elem;
+      }
+      return null;
+    };
+
+    var filtered = _.filter($things, filter);
+    var container = $('.filtered-things ul');
+
+    var removeChildren = function removeChildren(c) {
+      while (c.firstChild) {
+        c.removeChild(c.firstChild);
+      }
+    };
+
+    removeChildren(container);
+    _.map(filtered, function (elem) {
+      container.appendChild(elem);
+    });
+  };
+
+  var updateString = function updateString() {
+    var string = $('span.district');
+
+    string.classList.add('active');
+    string.innerText = district;
+  };
+
+  if (e.target && e.target.nodeName == 'IMG') {
+    updateThings();
+    updateString();
+  }
+});
+
+$('nav .districts.dropdown').addEventListener('click', function (e) {
+  $('div.districts').classList.toggle('hidden');
+});
+
+$('nav .activities.dropdown').addEventListener('click', function (e) {
+  $('div.activities').classList.toggle('hidden');
 });
 //# sourceMappingURL=main.js.map
